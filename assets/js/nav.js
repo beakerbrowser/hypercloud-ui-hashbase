42d9ca36d7bf26c3641bf095344ea6e4ddae5c15
Tara Vancil <tbvanc@gmail.com>
2017-04-22 14:16:43 -0500

    Add responsive mobile nav

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -4,8 +4,14 @@
 $(function () {
   var dropdownMenu = $('.nav .dropdown-menu')
   var dropdownMenuToggle = $('.nav .dropdown-menu-link')
+  var mobileNav = $('.mobile-nav')
+  var mobileNavToggle = $('.mobile-nav-toggle')
 
   dropdownMenuToggle.click(function () {
     dropdownMenu.toggleClass('open')
+  })
+
+  mobileNavToggle.click(function () {
+    mobileNav.toggleClass('open')
   })
 })
